b04541518ba821e5a72b490283e90a9efeb85e23
zenithbck-source <zenithbck@gmail.com>
2026-09-15 12:27:31 +0800

    Added SJF Policy

--- scheduler_utils.py
+++ scheduler_utils.py
@@ -6,11 +6,12 @@ def simulate_fcfs(jobs, total_cores):
     scheduled_jobs = []
     schedule = []
     avail_cores = total_cores
+    current_time = 1
 
-    for current_time in range(1, 200):
+    while jobs != []:
         for job in schedule:
-                    if job['end_time'] == current_time:
-                        avail_cores += job['ncpus']
+            if job['end_time'] == current_time:
+                avail_cores += job['ncpus']
 
         for job in jobs:
             if current_time >= job['submit_time'] and avail_cores >= job['ncpus']:
@@ -30,8 +31,49 @@ def simulate_fcfs(jobs, total_cores):
 
     return schedule
 
+
+def simulate_sjf(jobs, total_cores):
+    jobs = sorted(jobs, key=lambda job: job['runtime'])  # lower submit_time means earlier submission
+    scheduled_jobs = []
+    schedule = []
+    avail_cores = total_cores
+    current_time = 1
+
+    while jobs != []:
+        for job in schedule:
+            if job['end_time'] == current_time:
+                avail_cores += job['ncpus']
+
+        for job in jobs:
+            if current_time >= job['submit_time'] and avail_cores >= job['ncpus']:
+                start_time = current_time
+                wait_time = current_time - job['submit_time']
+                avail_cores -= job['ncpus']
+                scheduled_jobs.append(job['job_id'])
+                schedule.append({'job_id': job['job_id'], 'start_time': start_time, 'wait_time': wait_time, 'end_time': start_time + job['runtime'], 'ncpus': job['ncpus']})
+
+        jobs = [job for job in jobs if job['job_id'] not in scheduled_jobs]
+        
+        current_time += 1
+
+    for job in schedule:
+        job.pop('end_time', None)
+        job.pop('ncpus', None)
+
+    return schedule
+
+
+
 if __name__ == "__main__":
+    print("FCFS Policy Schedule:")
     jobs = generate_jobs(15)
-    schedule = simulate_fcfs(jobs, 4)
-    df = pd.DataFrame(schedule)
-    print(df)
+    schedule_fcfs = simulate_fcfs(jobs, 4)
+    df_fcfs = pd.DataFrame(schedule_fcfs)
+    print(df_fcfs)
+
+    print()
+
+    print("SJF Policy Test:")
+    schedule_sjf = simulate_sjf(jobs, 4)
+    df_sjf = pd.DataFrame(schedule_sjf)
+    print(df_sjf)
